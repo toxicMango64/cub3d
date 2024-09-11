7b0f9fa57e422d51c7fa286b4cc5c35c8206a5ba
toxicMango64 <124577028+toxicMango64@users.noreply.github.com>
2024-09-11 14:28:51 -0700

    test2 and test header file update

--- testing/test.h
+++ testing/test.h
@@ -1,62 +1,81 @@
-#ifndef  TEST_H
-# define  TEST_H
+#ifndef TEST_H
+# define TEST_H
 
-// strspn
+// // strspn
 
-#include "../zlibc/libft.h"
-#include "../utils/utils.h"
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+// #include "../zlibc/libft.h"
+// #include "../utils/utils.h"
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
 
 # define WHITESPACE	" \v\t\f\r\n"
 
-/** STRUCTURES **/
-typedef struct s_game			t_soul_catcher;
-typedef struct s_map			t_map;
-typedef struct s_mlx			t_mlx;
-typedef struct s_player			t_player;
+# ifndef BONUS
+#  define BONUS 0
+# endif
 
-struct	s_player
-{
-	int		x;
-	int		y;
-	char	direction;
-};
+# if BONUS
 
-struct	s_map
+int	usage_msg(char **av)
 {
-	int			fd;
-	int			width;
-	int			floor_color[3];
-	int			ceiling_color[3];
-	char		**grid;
-	char		*texture[7];
-	t_player	*player_pos;
-};
+	printf("BONUS!!\n");
+	return (printf("Usage: %s <map_file>\n", av[0]), 1);
+}
+# else
 
-/**
- * mx mouse x cordinates
- * my mouse y cordinates
- */
-struct	s_mlx
+int	usage_msg(char **av)
 {
-	int	win_w;
-	int	win_h;
-	int	mx;
-	int	my;
-	int	forward;
-	int	backward;
-	int	left;
-	int	right;
-};
+	return (printf("Usage: %s <map_file>\n", av[0]), 1);
+}
+# endif
 
-struct	s_game
-{
-	t_mlx	*mlx;
-	t_map	*map;
-};
+// /** STRUCTURES **/
+// typedef struct s_game			t_soul_catcher;
+// typedef struct s_map			t_map;
+// typedef struct s_mlx			t_mlx;
+// typedef struct s_player			t_player;
+
+// struct	s_player
+// {
+// 	int		x;
+// 	int		y;
+// 	char	direction;
+// };
+
+// struct	s_map
+// {
+// 	int			fd;
+// 	int			width;
+// 	int			floor_color[3];
+// 	int			ceiling_color[3];
+// 	char		**grid;
+// 	char		*texture[7];
+// 	t_player	*player_pos;
+// };
+
+// /**
+//  * mx mouse x cordinates
+//  * my mouse y cordinates
+//  */
+// struct	s_mlx
+// {
+// 	int	win_w;
+// 	int	win_h;
+// 	int	mx;
+// 	int	my;
+// 	int	forward;
+// 	int	backward;
+// 	int	left;
+// 	int	right;
+// };
+
+// struct	s_game
+// {
+// 	t_mlx	*mlx;
+// 	t_map	*map;
+// };
 
 #endif
--- testing/test2.c
+++ testing/test2.c
@@ -1,12 +1,19 @@
-#include <stdio.h>
+#include "test.h"
 
 int main(int ac, char **av)
 {
-	(void)ac;
-	char *line = av[1];
-	if (line)
-		printf("line is: {%s}\n", line);
-	else if (!line)
-		printf("line is null: {%s}\n", line);
-	return (0);
+	if (2 != ac)
+		return (usage_msg(av));
+	return (EXIT_SUCCESS);
 }
+
+// int main(int ac, char **av)
+// {
+// 	(void)ac;
+// 	char *line = av[1];
+// 	if (line)
+// 		printf("line is: {%s}\n", line);
+// 	else if (!line)
+// 		printf("line is null: {%s}\n", line);
+// 	return (0);
+// }
